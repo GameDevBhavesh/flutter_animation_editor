a5772674766b6deab34d495e6cd65d476f6399fa
bhavesh tiwari <bhavesh@techphant.com>
2023-10-09 17:32:00 +0530

    page

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,20 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/bhavesh/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/bhavesh/Work/officeProjects/packages/animation_editor" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/bhavesh/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/bhavesh/Work/officeProjects/packages/animation_editor"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=true"
+  "PACKAGE_CONFIG=/home/bhavesh/Work/officeProjects/packages/animation_editor/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=example/main.dart"
+)
