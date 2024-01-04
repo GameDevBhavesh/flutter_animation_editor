3d31ac80b305d2afdec4adfb5a57ad2795a5d45b
GameDevBhavesh <btiwari441@gmail.com>
2024-01-05 03:23:34 +0530

    update

--- lib/animation_editor/widgets/tracked_widget.dart
+++ lib/animation_editor/widgets/tracked_widget.dart
@@ -5,20 +5,20 @@ import 'package:flutter/material.dart';
 import 'package:state_managment/state_magment.dart';
 
 class TrackedWidget extends StatelessWidget {
-  const TrackedWidget(
-      {super.key,
-      required this.id,
-      required this.builder,
-      this.isAnimating = true});
+  const TrackedWidget({
+    super.key,
+    required this.id,
+    required this.builder,
+  });
   final String id;
   final Widget Function(BuildContext context) builder;
-  final bool isAnimating;
+
   @override
   Widget build(BuildContext context) {
     final editor = ControllerQuery.of<TrackedAnimationController>(context);
-    if (editor != null && isAnimating) {
-      final objController = editor.readChildController(id)!;
-      if (objController == null) {
+    if (editor != null) {
+      final objController = editor.readChildController(id);
+      if (objController != null) {
         return ControllerQuery<ObjectTrackController>(
             controller: objController,
             child: ListenableBuilder(
